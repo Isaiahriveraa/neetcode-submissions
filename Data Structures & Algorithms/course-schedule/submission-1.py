56c67d6aa12a4439bca901211581a6aa32dc50c5
Yonie Rivera-Aguilar <yonieisaiahriveraaguilar@gmail.com>
2026-07-06 21:56:46 -0700

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        # we must take 2 courses
+        adj_list = [[] for _ in range(numCourses)]
+        # must take course b before we take a meaning b should have the nei of a
+        num_preq_needed = [0] * numCourses
+
+        for pre, crs in prerequisites:
+            adj_list[crs].append(pre)
+            num_preq_needed[pre] += 1
+        
+        q = deque()
+
+        for i, node in enumerate(num_preq_needed):
+            if node == 0:
+                q.append(i)
+
+        finish = 0
+        while q:
+            node = q.popleft()
+            finish += 1 
+
+            for nei in adj_list[node]:
+                num_preq_needed[nei] -= 1
+                if num_preq_needed[nei] == 0:
+                    q.append(nei)
+                
+        
+        return finish == numCourses
+
