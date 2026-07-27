1ce0aed8076c24bab4c51974667a618c66e46b9e
Yonie Rivera-Aguilar <yonieisaiahriveraaguilar@gmail.com>
2026-07-26 17:16:23 -0700

    Add: remove-sub-folders-from-the-filesystem - submission-3

---
+++ Data Structures & Algorithms/remove-sub-folders-from-the-filesystem/submission-3.py
@@ -0,0 +1,57 @@
+class TrieNode:
+  
+    # instructor (self, is_end_of_folder, childern_folders)
+    def __init__(self, is_end_of_folder=False) -> None:
+        self.is_end_of_folder = is_end_of_folder
+        self.children = defaultdict(TrieNode)
+        pass
+    
+    def add(self, words):
+        # we are given a list of words that we must add to the Trie
+        # process the words one by one
+        # start at the root
+        cur = self
+        for word in words:
+            # word is not in children -> its created automatically by defaultdict
+            # traverse to that child
+            cur = cur.children[word]
+        
+        # MARK as end of folder
+        cur.is_end_of_folder = True
+
+        return None
+    
+    def traverse(self):
+
+        root = self
+        res = []
+        # Goal is to traverse the tree and if we hit a 
+        def look_for_end_of_folders(cur, word):
+            if not cur:
+                return 
+            
+            if cur.is_end_of_folder:
+                res.append(word)
+                return
+            
+            for child in cur.children:
+                look_for_end_of_folders(cur.children[child], word + "/" + child)
+            
+            return
+        look_for_end_of_folders(root, '')
+        return res
+
+class Solution:
+
+    def removeSubfolders(self, folders: List[str]) -> List[str]:
+        root = TrieNode()
+        
+        # Create the Trie
+        for folder in folders:
+            clean_folder = folder.strip('/').split('/')
+            root.add(clean_folder)
+
+        # Gather the folders
+        
+        return root.traverse() # call the TrieNode class reverse method
+
